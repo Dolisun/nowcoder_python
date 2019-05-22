8d1e1360d5dc2d0fd0c5842a4587a10a7fa92373
rancho <35914228+TimYann@users.noreply.github.com>
2019-05-22 23:47:33 +0800

    重建二叉树

---
+++ 005_用两个栈实现队列.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+    def push(self, node):
+        # write code here
+        self.stack1.append(node)
+
+    def pop(self):
+        # return xx
+        if self.stack1 == []:
+            return None
+        for i in range(len(self.stack1)):
+            self.stack2.append(self.stack1.pop())
+        out = self.stack2.pop()
+        for i in range(len(self.stack2)):
+            self.stack1.append(self.stack2.pop())
+        return out
