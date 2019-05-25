900fec984c25ea98b9748d7624d14fa5e9e78278
rancho <35914228+TimYann@users.noreply.github.com>
2019-05-25 13:20:26 +0800

    矩形覆盖

---
+++ 010_矩形覆盖.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+class Solution:
+    def rectCover(self, number):
+        # write code here
+        if number == 0:
+            return 0
+        if number == 1:
+            return 1
+        if number == 2:
+            return 2
+        f1, f2 = 1, 2
+        for i in range(3, number+1):
+            f3 = f1 + f2
+            f1 = f2
+            f2 = f3
+        return f3
